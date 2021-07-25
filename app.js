392016d3254eb6340443ec6be8dc7ce49f5f460a
Sperthix <matusbuci@gmail.com>
2021-07-25 13:38:16 +0200

    pridany druhy zoznam = aktualny nakup. Dokoncena logika, hotofo

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const addItem = document.querySelector('.add');
-const list = document.querySelector('.todos');
+const list = document.querySelector('.nakupnyZoznam');
+const shoppingCart = document.querySelector('.vKosiku');
 const searchField = document.querySelector('.search input');
+const clear = document.querySelector('.nakupene');
 
+
+//vytvaranie polozky do nakupneho zoznamu
 const createLi = newItem => {
 
     const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
         <span>${newItem}</span>
-        <i class="far fa-trash-alt delete"></i>
+        <span>
+            <i class="addToCart fas fa-cart-plus"></i>
+            <i class="delete fas fa-trash"></i>
+        </span>
     </li>
     `;
     list.innerHTML += html;
+}
+
+//vytvaranie polozky do kosika
+const createLiCart = newItem => {
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${newItem}</span>
+        <i class="delete fas fa-trash"></i>
+    </li>
+    `;
+    shoppingCart.innerHTML += html;
 }
 
 
@@ -28,9 +46,22 @@ addItem.addEventListener('submit', e => {
 
 list.addEventListener('click', e => {
     if(e.target.classList.contains('delete')){
+        e.target.parentElement.parentElement.remove();
+    }
+    if(e.target.classList.contains('addToCart')){
+        createLiCart(e.target.parentElement.parentElement.textContent.trim());
+        e.target.parentElement.parentElement.remove();
+    }
+})
+
+
+shoppingCart.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')){
+        createLi(e.target.parentElement.textContent.trim());
         e.target.parentElement.remove();
     }
 })
+
 //prehladavanie zoznamu
 
 const filterList = (phrase) => {
@@ -46,4 +77,13 @@ const filterList = (phrase) => {
 searchField.addEventListener('keyup', () => {
     const phrase = searchField.value.trim().toLowerCase();
     filterList(phrase);
+})
+
+//dokoncenie nakupu
+
+clear.addEventListener('click', e => {
+    console.log('klikaaaam');
+    while(shoppingCart.hasChildNodes()){
+        shoppingCart.removeChild(shoppingCart.firstChild);
+    }
 })
